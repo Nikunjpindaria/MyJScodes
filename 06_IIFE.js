776fee0a0a527c99c512b99730d404755ad1487a
Nikunjpindaria <editsytmanage@gmail.com>
2026-05-15 04:51:18 +0000

    DOM

--- 06_IIFE.js
+++ 06_IIFE.js
@@ -3,8 +3,8 @@ const nikunj = (num1,num2)=>({name:'hitesh'})
 
 (function nikunj(){
     console.log("hello")
-})();  //--> named IIFE --> (;) requres for two IIFE in one code
+    })();  //--> named IIFE --> (;) requres for two IIFE in one code
 
 
-( (name) => { 
-    console.log("hello"+name)} )("nikunj") //--> thats how parameters passed
+// ( (name) => { 
+//     console.log("hello"+name)} )("nikunj") //--> thats how parameters passed
